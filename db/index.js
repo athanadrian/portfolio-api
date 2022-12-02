d1f11311ba4b5a116e948463fabec81df29e7cba
athanadrian <athanadrian@gmail.com>
2022-12-02 21:43:48 +0200

    fix

--- db/index.js
+++ db/index.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
-const config = require('../config');
+//const config = require('../config');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const connectDB = async () => {
-  const conn = await mongoose.connect(config.MONGO_URI, {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
